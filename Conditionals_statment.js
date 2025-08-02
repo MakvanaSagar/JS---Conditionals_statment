16c0fceb3c8ca933efa4c672add3d2ef25cbfd46
Makvana Sagar <134779010+MakvanaSagar@users.noreply.github.com>
2025-08-02 21:53:17 +0530

    Add files via upload

---
+++ Conditionals_statment.js
@@ -0,0 +1,59 @@
+/* ________________if-else________________
+
+let age = 19;
+
+if(age >= 18){
+	console.log("You can vote!");
+}
+else{
+	console.log("You can not vote!");
+}
+*/
+
+/*__________________if-else-if____________
+
+let mark = 59;
+
+if(mark>=61 && mark<=70){
+	console.log("Grade : AA");
+}
+else if(mark>=51 && mark<=60){
+	console.log("Grade : BB");
+}
+else if(mark>=41 && mark<=50){
+	console.log("Grade : CC");
+}
+else if(mark>=23 && mark<=40){
+	console.log("Grade : DD");
+}
+else{
+	console.log("Grade : FF")
+}
+*/
+
+/*_________________switch case_____________
+
+let num = 19;
+
+switch(num){
+	case 18: console.log("A");
+	break;
+	case 19: console.log("B");
+	break;
+	case 20: console.log("C");
+
+
+}
+
+let name = "Sagar";
+
+switch(name){
+	case "Sagar": console.log("A");
+	break;
+	case "Om": console.log("B");
+	break;
+	case "Priyant": console.log("C");
+
+
+}
+*/
